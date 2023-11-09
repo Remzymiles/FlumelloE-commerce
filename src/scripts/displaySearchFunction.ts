d7ea808bf24bac21039b28f96972303881a851ff
SomtoDev <remzymiles@gmail.com>
2023-11-09 10:23:01 +0000

    added highlight for the search inputs

--- src/scripts/displaySearchFunction.ts
+++ src/scripts/displaySearchFunction.ts
@@ -7,51 +7,54 @@ export const displaySearchedProducts = (
   products: IProduct[],
   userInput: string,
   theHtmlElement: { innerHTML: string },
-  errorMsg: { innerHTML: string }) => {
-
+  errorMsg: { innerHTML: string }
+) => {
   const matchingProducts = products.filter(
-      (product) =>
-      product.title.toLowerCase().split(" ").join("").includes(userInput) ||
-      product.brand.toLowerCase().split(" ").join("").includes(userInput) ||
-      product.category.toLowerCase().split(" ").join("").includes(userInput)
-      );
-      
-      
-  
-    if (matchingProducts.length > 0) {
-      let showSearchProducts = "";
-  
-      matchingProducts.forEach((product) => {
-        showSearchProducts += `
-              <a href="product-page.html" class = "click">
-                  <!--  -->
-                  <div class="search_card_image">
-                    <img src="${product.images[0]}" alt=""/>
-                  </div>
-                  <!--  -->
-                  <div class="search_card_description">
-                    <h4>${product.title}</h4>
-                    <h4 class= "product_id">${product.id}</h4>
-                     <div class="search_card_price">
-                      <span class="">₦${(product.price * 520).toLocaleString()}</span>
-                      <span class="search_card_line_through">₦ ${Math.floor((product.price / (1 - (product.discountPercentage / 100)) * 520)).toLocaleString()}</span>
-                    </div>
-                  </div>
-                  <!--  -->
-                </a>
-              `;
+    (product) =>
+      product.title.toLowerCase().includes(userInput) ||
+      product.brand.toLowerCase().includes(userInput) ||
+      product.category.toLowerCase().includes(userInput)
+  );
+
+  if (matchingProducts.length > 0) {
+    let showSearchProducts = "";
+
+    matchingProducts.forEach((product) => {
+      const highlightedTitle = highlightMatch(userInput, product.title);
+
+      showSearchProducts += `
+        <a href="product-page.html" class="click">
+          <div class="search_card_image">
+            <img src="${product.images[0]}" alt=""/>
+          </div>
+          <div class="search_card_description">
+            <h4>${highlightedTitle}</h4>
+            <h4 class="product_id">${product.id}</h4>
+            <div class="search_card_price">
+              <span class="">₦${(product.price * 520).toLocaleString()}</span>
+              <span class="search_card_line_through">₦ ${Math.floor((product.price / (1 - (product.discountPercentage / 100)) * 520)).toLocaleString()}</span>
+            </div>
+          </div>
+        </a>
+      `;
+    });
+
+    theHtmlElement.innerHTML = showSearchProducts;
+
+    const productCards = document.querySelectorAll(".click");
+    productCards.forEach((product) => {
+      product.addEventListener("click", () => {
+        const productId = product.querySelector(".product_id").textContent;
+        localStorage.setItem("clickedProductId", JSON.stringify(productId));
       });
-      theHtmlElement.innerHTML = showSearchProducts;
-  
-      const productCards = document.querySelectorAll(".click");
-      productCards.forEach((product) => {
-        product.addEventListener("click", () => {
-          const productId = product.querySelector(".product_id").textContent;
-          localStorage.setItem("clickedProductId", JSON.stringify(productId));
-        });
-      })  
-  
-    } else {
-        errorMsg.innerHTML = `<p>Opps! Product not available</p>`;
-    }
-  };
+    });
+  } else {
+    errorMsg.innerHTML = `<p>Oops! Product not available</p>`;
+  }
+};
+
+const highlightMatch = (userInput: string, text: string) => {
+  const regex = new RegExp(`(${userInput})`, 'gi');
+  return text.replace(regex, (match) => `<span class="highlight">${match}</span>`);
+};
+;
